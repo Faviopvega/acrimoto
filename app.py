94d61a63175f09d75cf8677090f69b1dddbb9c22
yahirBastos <bastosy@uninorte.edu.co>
2021-10-21 16:38:14 -0500

    VERSION 0.3
    templates, rutas, jinga, css, bootstrap 5 se modifico.

--- app.py
+++ app.py
@@ -1,6 +1,23 @@
 from flask import Flask, render_template
+import os
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return render_template("index.html")
+
+@app.route('/login')
+def login():
+    return render_template("login.html")
+
+@app.route('/inicioSesion', methods=['GET','POST'])
+def inicioSesion():
+    return render_template("login.html")
+    
+
+
+
+if  __name__ == "__main__":
+    os.environ['FLASK_ENV'] = 'development'
+    app.run(debug=True)
